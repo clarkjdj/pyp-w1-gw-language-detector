6e6e74b0abad312a6e95dfc7e002f802f119a8d9
i am DCJ <clarkjd.j@gmail.com>
2016-05-20 12:44:22 +0000

    Preliminary files for languages.py and main.py

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -1,9 +1,41 @@
+# -*- coding: UTF=8 -*-
 """This file contains the definition of all languages supported by the program."""
 
 LANGUAGES = [
     {
         'name': 'Spanish',
-        'common_words': ['yo', 'tu', 'el', 'nosotros']  # add your words here
+        'common_words': ['el', 'la', 'de', 'que', 'y', 'a', 'en', 'un', 'ser', 'se',
+                    'no', 'haber', 'por', 'con', 'su', 'para', 'como', 'estar',
+                    'tener', 'le', 'lo', 'lo', 'todo', 'pero', 'más', 'hacer',
+                    'o', 'poder', 'decir', 'este', 'ir', 'otro', 'ese', 'la',
+                    'si', 'me', 'ya', 'ver', 'porque', 'dar', 'cuando', 'él',
+                    'muy', 'sin', 'vez', 'mucho', 'saber', 'qué', 'sobre',
+                    'mi', 'alguno', 'mismo', 'yo', 'también', 'hasta']  # add your words here
     },
     #  include your extra languages here
+    {
+        'name': 'German',
+        'common_words': [ 'das', 'ist', 'du', 'ich', 'nicht', 'die', 'es', 'und',
+                    'der', 'was', 'wir', 'zu', 'ein', 'er', 'in', 'sie', 'mir',
+                    'mit', 'ja', 'wie', 'den', 'auf', 'mich', 'dass', 'so',
+                    'hier', 'eine', 'wenn', 'hat', 'all', 'sind', 'von',
+                    'dich', 'war', 'haben', 'für', 'an', 'habe', 'da', 'nein',
+                    'bin', 'noch', 'dir', 'uns', 'sich', 'nur',
+                    'einen', 'kann', 'dem']  # add your words here
+    },
+    
+    {
+        'name': 'English',
+        'common_words': ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 
+                    'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 
+                    'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 
+                    'they', 'we', 'say', 'her', 'she', 'or', 'an', 'will', 'my',
+                    'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up',
+                    'out', 'if', 'about', 'who', 'get', 'which', 'go']  
+                    #English words from wikipedia https://en.wikipedia.org/wiki/Most_common_words_in_English
+    }
 ]
+
+
+
+
--- language_detector/main.py
+++ language_detector/main.py
@@ -2,8 +2,37 @@
 
 from .languages import LANGUAGES
 
+def score_text(list_of_words, language):
+    counter = 0
+    # go one at a time through each word in text
+        # if that word is in the most common word list, gets +1
+    for word in list_of_words:
+        # if that word is in the most common word list, gets +1
+        if word in language['common_words']:
+            counter += 1
+    return counter
+    
 
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
+
+    scores = []
+    
+    #Make sure inbound text is lowercase
+    text = text.lower()
+    #for each language appends a tuple of (score, language name)
+    #Calls score_text to determine score
+    for language in languages:
+        scores.append((score_text(text.split(), language), language['name']))
+        
+    
+    scores.sort()
+    #return the highest sorted score-tuple at position -1, and return language name -- the 2nd tuple item
+    return scores[-1][1]
+      
+
+#==================================
+
+#def detect_language(text, languages=LANGUAGES):
+#   """Returns the detected language of given text."""
     # implement your solution here
-    pass
